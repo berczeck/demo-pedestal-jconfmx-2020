b1229b2f1a6ab8ec35109e37cd55a7670f7fd929
Alex Aldazabal <alex.aldazabal@nubank.com.br>
2020-10-01 02:33:33 -0500

    interceptor added

--- src/demo_pedestal_jconfmx_2020/route.clj
+++ src/demo_pedestal_jconfmx_2020/route.clj
@@ -5,10 +5,18 @@
 						[clojure.string :as str]
 						[demo-pedestal-jconfmx-2020.util.content :refer [to->json]]))
 
-;; TODO: Create demo-interceptor, add it to routes
+
+;; context {:request {:path-params {:name}}}
+;; context {:response {:headers {"Content-Type"} :body}}
+(def demo-interceptor
+	{:name :demo-interceptor
+	 :enter (fn [context]
+						(update-in context [:request :path-params :name] #(str/replace % #"%20" "-")))
+	 :leave (fn [context]
+						(update-in context [:response] to->json))})
 
 (def routes
 	(route/expand-routes
 		#{["/hello-world" :get [service/hello-world] :route-name :hello-world]
-			["/dragon-ball/:name" :get [service/dragon-ball-view] :route-name :dragon-ball-view]
-			["/dragon-ball/:name":post [(body-params/body-params) service/dragon-ball-create] :route-name :dragon-ball-create]}))
+			["/dragon-ball/:name" :get [demo-interceptor service/dragon-ball-view] :route-name :dragon-ball-view]
+			["/dragon-ball/:name":post [demo-interceptor (body-params/body-params) service/dragon-ball-create] :route-name :dragon-ball-create]}))
