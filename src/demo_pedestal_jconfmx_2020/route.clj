b064f55631734a52ba3a00055b217905b6fda932
Luis Alexander Aldazabal Gil <alexaldazabal@outlook.com>
2020-10-01 02:02:42 -0500

    Step1 - Hello world (#1)
    * Hello-world route ceated
    
    * TODO removed

--- src/demo_pedestal_jconfmx_2020/route.clj
+++ src/demo_pedestal_jconfmx_2020/route.clj
@@ -1,11 +1,16 @@
 (ns demo-pedestal-jconfmx-2020.route
 	(:require [io.pedestal.http.body-params :as body-params]
+						[io.pedestal.http.route :as route]
+						[demo-pedestal-jconfmx-2020.service :as service]
 						[clojure.string :as str]
 						[demo-pedestal-jconfmx-2020.util.content :refer [to->json]]))
 
-;; TODO: Create hello-world route GET "/hello-world" & handler
 ;; TODO: Create dragon-ball-view route GET "/dragon-ball/:name" & read handler
 ;; TODO: Create dragon-ball-create route POST "/dragon-ball/:name" & create handler (using get)
 ;; TODO: Refactor create handler using let (path-params json-params)
 ;; TODO: Refactor create handler using destructuring {:keys [json-params path-params]}
 ;; TODO: Create demo-interceptor, add it to routes
+
+(def routes
+	(route/expand-routes
+		#{["/hello-world" :get [service/hello-world] :route-name :hello-world]}))
--- src/demo_pedestal_jconfmx_2020/service.clj
+++ src/demo_pedestal_jconfmx_2020/service.clj
@@ -1,2 +1,6 @@
 (ns demo-pedestal-jconfmx-2020.service
 	(:require [demo-pedestal-jconfmx-2020.util.data :as data]))
+
+(defn hello-world
+	[_]
+	{:status 200 :body "Hello, world!"})
