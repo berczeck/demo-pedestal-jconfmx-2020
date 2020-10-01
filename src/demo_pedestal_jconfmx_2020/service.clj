9af51f75df13c55a030208fd944b4b185a014fb3
Luis Alexander Aldazabal Gil <alexaldazabal@outlook.com>
2020-10-01 02:22:58 -0500

    dragon-ball-create route added (#3)

--- src/demo_pedestal_jconfmx_2020/service.clj
+++ src/demo_pedestal_jconfmx_2020/service.clj
@@ -9,3 +9,9 @@
 (defn dragon-ball-view
 	[request]
 	{:status 200 :body (data/read! (:name (:path-params request)))})
+
+;; request {:path-params {:name} :json-params {:power :friends}}
+(defn dragon-ball-create
+	[request]
+	{:status 200
+	 :body (data/create! (get-in request [:path-params :name]) (get-in request [:json-params :power]) (get-in request [:json-params :friends]))})
